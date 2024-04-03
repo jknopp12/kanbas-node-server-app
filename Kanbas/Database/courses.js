3baf20bfa214cb7797d4d9ad54dbc47ac8732f79
jknopp12 <129903306+jknopp12@users.noreply.github.com>
2024-04-03 15:38:44 -0400

    first commit

---
+++ Kanbas/Database/courses.js
@@ -0,0 +1,66 @@
+export default[
+    {
+        "_id": "CS2500",
+        "name": "Computer Science",
+        "number": "CS2500",
+        "startDate": "2024-01-10",
+        "endDate": "2024-05-15",
+        "image": "cs.png"
+    },
+    {
+        "_id": "HIST1100",
+        "name": "History of the World",
+        "number": "HIST1100",
+        "startDate": "2023-01-10",
+        "endDate": "2023-05-15",
+        "image": "history.jpg"
+    },
+    {
+        "_id": "INSH2102",
+        "name": "Bostonography",
+        "number": "INSH2102",
+        "startDate": "2023-01-10",
+        "endDate": "2023-05-15",
+        "image": "city.jpg"
+    },
+    {
+        "_id": "HIST2310",
+        "name": "American History",
+        "number": "HIST2310",
+        "startDate": "2023-01-10",
+        "endDate": "2023-05-15",
+        "image": "history2.jpg"
+    },
+    {
+        "_id": "MATH3300",
+        "name": "Calculus",
+        "number": "MATH3300",
+        "startDate": "2023-01-10",
+        "endDate": "2023-05-15",
+        "image": "math.jpg"
+    },
+    {
+        "_id": "SCIE1000",
+        "name": "Intro to Life Sciences",
+        "number": "SCIE1000",
+        "startDate": "2023-01-10",
+        "endDate": "2023-05-15",
+        "image": "science.jpg"
+    },
+    {
+        "_id": "MATH1560",
+        "name": "Statistics",
+        "number": "MATH1560",
+        "startDate": "2023-01-10",
+        "endDate": "2023-05-15",
+        "image": "statistics.png"
+    },
+    {
+        "_id": "CS1111",
+        "name": "Co-op Development",
+        "number": "CS1111",
+        "startDate": "2023-01-10",
+        "endDate": "2023-05-15",
+        "image": "coop.webp"
+    }
+]
---
+++ Kanbas/Database/modules.js
@@ -0,0 +1,599 @@
+export default[
+    {
+      "_id": "M102",
+      "name": "Course Resources",
+      "description": "A list of resources to help get you through class",
+      "course": "CS2500",
+      "lessons": [
+        {
+          "_id": "L101",
+          "name": "Syllabus: CS2500 Spring 2024",
+          "description": "Course Syllabus for this term",
+          "module": "M102"
+        },
+        {
+          "_id": "L102",
+          "name": "Office Hours Schedule",
+          "description": "Schedule for when office hours are offered",
+          "module": "M102"
+        },
+        {
+          "_id": "L103",
+          "name": "GitHub",
+          "description": "GitHub to reference",
+          "module": "M102"
+        },
+        {
+          "_id": "L103",
+          "name": "Textbook",
+          "description": "textbook that will be helpful for learning",
+          "module": "M102"
+        }
+      ]
+    },
+    {
+      "_id": "M103",
+      "name": "Week 1 - Course Introduction, Syllabus, Agenda, Etc.",
+      "description": "Understanding basic principles of coding and into to the Dr. Racket Programming Language.",
+      "course": "CS2500",
+      "lessons": [
+        {
+          "_id": "L201",
+          "name": "Learning Objective: Understanding what will be taught in this course",
+          "description": "Overview of what will be taught in this class.",
+          "module": "M102"
+        },
+        {
+          "_id": "L202",
+          "name": "Readings: Chapters 1 and 2",
+          "description": "Supplemental readings for this week.",
+          "module": "M102"
+        },
+        {
+          "_id": "L203",
+          "name": "Slides: Intro to Fundamentals of Computer Science",
+          "description": "Intro to Programming.",
+          "module": "M102"
+        }
+      ]
+    },
+    {
+      "_id": "M104",
+      "name": "Week 2 - Reviewing Data Types",
+      "description": "Understanding data types to set a foundation for the course.",
+      "course": "CS2500",
+      "lessons": [
+        {
+          "_id": "L201",
+          "name": "Learning Objective: Learn about various data types to serve as a basis for the course",
+          "description": "Overview of data types.",
+          "module": "M102"
+        },
+        {
+          "_id": "L202",
+          "name": "Readings: Chapters 3 and 4",
+          "description": "Supplemental readings for this week.",
+          "module": "M102"
+        },
+        {
+          "_id": "L203",
+          "name": "Slides: Data Types",
+          "description": "Data Types.",
+          "module": "M102"
+        }
+      ]
+    },
+    {
+      "_id": "M105",
+      "name": "Week 3 - Starting to Write Code!",
+      "description": "Understanding basic principles of coding and beginning to write code.",
+      "course": "CS2500",
+      "lessons": [
+        {
+          "_id": "L201",
+          "name": "Learning Objective: Begin understanding and writing code to begin working on assignment 1",
+          "description": "Begin writing code for the first project",
+          "module": "M102"
+        },
+        {
+          "_id": "L202",
+          "name": "Readings: Chapters 4 and 5",
+          "description": "Supplemental readings for this week.",
+          "module": "M102"
+        },
+        {
+          "_id": "L203",
+          "name": "Slides: Writing Functions",
+          "description": "Writing Functions.",
+          "module": "M102"
+        }
+      ]
+    },
+    {
+      "_id": "M106",
+      "name": "Week 1: Welcome!",
+      "description": "Intro to Course",
+      "course": "HIST1100",
+      "lessons": [
+        {
+          "_id": "L301",
+          "name": "Course Syllabus - HIST1100",
+          "description": "Syllabus for the course",
+          "module": "M103"
+        },
+        {
+          "_id": "L302",
+          "name": "Office Hours - When & Where?",
+          "description": "When and where office hours will be held",
+          "module": "M103"
+        },
+        {
+          "_id": "L303",
+          "name": "University Policy",
+          "description": "code of conduct",
+          "module": "M103"
+        }
+      ]
+    },
+    {
+      "_id": "M107",
+      "name": "Week 2: The Very Beginning - Early History",
+      "description": "Review of early history",
+      "course": "HIST1100",
+      "lessons": [
+        {
+          "_id": "L301",
+          "name": "The Stone Age",
+          "description": "the stone age",
+          "module": "M103"
+        },
+        {
+          "_id": "L302",
+          "name": "Evolution",
+          "description": "evolution",
+          "module": "M103"
+        },
+        {
+          "_id": "L303",
+          "name": "Early Historical Figures",
+          "description": "famous people",
+          "module": "M103"
+        }
+      ]
+    },
+    {
+      "_id": "M208",
+      "name": "Welcome to Bostonography! - Course Introduction",
+      "description": "Course Intro",
+      "course": "INSH2102",
+      "lessons": [
+        {
+          "_id": "L301",
+          "name": "Overview of the Course",
+          "description": "the stone age",
+          "module": "M202"
+        },
+        {
+          "_id": "L302",
+          "name": "Syllabus - Office Hours, Grading, Academic Policy, and More",
+          "description": "evolution",
+          "module": "M202"
+        }
+      ]
+    },
+    {
+      "_id": "M209",
+      "name": "The Revolutionary War - History, Historical Figures, and the Freedom Trail",
+      "description": "Boston's relevance in the revolution",
+      "course": "INSH2102"
+    },
+    {
+      "_id": "M210",
+      "name": "Urban Geography - Understand Boston's Neighborhoods",
+      "description": "understanding the geography of boston",
+      "course": "INSH2102"
+    },
+
+    {
+      "_id": "M2311",
+      "name": "Week 1: Welcome!",
+      "description": "Intro to American History",
+      "course": "HIST2310",
+      "lessons": [
+        {
+          "_id": "L301",
+          "name": "Course Syllabus",
+          "description": "Syllabus for the course",
+          "module": "M2310"
+        },
+        {
+          "_id": "L302",
+          "name": "Office Hours",
+          "description": "When and where office hours will be held",
+          "module": "M2310"
+        }
+      ]
+    },
+    {
+      "_id": "M2312",
+      "name": "Week 2: Early American History",
+      "description": "Review of early history",
+      "course": "HIST2310",
+      "lessons": [
+        {
+          "_id": "L301",
+          "name": "Roanoke",
+          "description": "About the Roanoke colony",
+          "module": "M2310"
+        },
+        {
+          "_id": "L302",
+          "name": "Jamestown",
+          "description": "About the Jamestown colony",
+          "module": "M2310"
+        },
+        {
+          "_id": "L303",
+          "name": "Colonial Life",
+          "description": "All about colonial life",
+          "module": "M2310"
+        }
+      ]
+    },
+    {
+      "_id": "M2313",
+      "name": "Week 3: Leading up to the Revolution",
+      "description": "Review of early history",
+      "course": "HIST2310",
+      "lessons": [
+        {
+          "_id": "L301",
+          "name": "The Stamp Act and Townshend Acts",
+          "description": "The Stamp Act and Townshend Acts",
+          "module": "M2310"
+        },
+        {
+          "_id": "L302",
+          "name": "The Boston Massacre",
+          "description": "The Boston Massacre",
+          "module": "M2310"
+        },
+        {
+          "_id": "L303",
+          "name": "The Boston Tea Party",
+          "description": "The Boston Tea Party",
+          "module": "M2310"
+        }
+      ]
+    },
+
+
+    {
+      "_id": "M23314",
+      "name": "WEEK 1: INTRO TO COURSE MATERIALS",
+      "description": "Intro to Calculus",
+      "course": "MATH3300",
+      "lessons": [
+        {
+          "_id": "L301",
+          "name": "Course Syllabus",
+          "description": "Syllabus for the course",
+          "module": "M23300"
+        },
+        {
+          "_id": "L302",
+          "name": "Office Hours",
+          "description": "When and where office hours will be held",
+          "module": "M23300"
+        },
+        {
+          "_id": "L302",
+          "name": "Textbook: Foundations of Calculus",
+          "description": "When and where office hours will be held",
+          "module": "M23300"
+        }
+      ]
+    },
+    {
+      "_id": "M23315",
+      "name": "WEEK 2: DERIVATIVES",
+      "description": "derivatives",
+      "course": "MATH3300",
+      "lessons": [
+        {
+          "_id": "L301",
+          "name": "How to do Derivatives",
+          "description": "About the Roanoke colony",
+          "module": "M23300"
+        },
+        {
+          "_id": "L302",
+          "name": "Homework 1",
+          "description": "homework 1",
+          "module": "M23300"
+        },
+        {
+          "_id": "L303",
+          "name": "Quiz 1: Simple Derivatives",
+          "description": "quiz",
+          "module": "M23300"
+        }
+      ]
+    },
+    {
+      "_id": "M23316",
+      "name": "WEEK 3: INTEGRATION",
+      "description": "integration",
+      "course": "MATH3300",
+      "lessons": [
+        {
+          "_id": "L301",
+          "name": "How to Integrate",
+          "description": "integrate",
+          "module": "M2310"
+        },
+        {
+          "_id": "L302",
+          "name": "Homework 2",
+          "description": "homework 2",
+          "module": "M23300"
+        },
+        {
+          "_id": "L303",
+          "name": "Quiz 2: Simple Integration",
+          "description": "quiz",
+          "module": "M23300"
+        }
+      ]
+    },
+
+    {
+      "_id": "M1517",
+      "name": "Intro to Statistics",
+      "description": "Intro to stats",
+      "course": "MATH1560",
+      "lessons": [
+        {
+          "_id": "L301",
+          "name": "Course Syllabus",
+          "description": "Syllabus for the course",
+          "module": "M1560"
+        },
+        {
+          "_id": "L302",
+          "name": "Office Hours",
+          "description": "When and where office hours will be held",
+          "module": "M1560"
+        },
+        {
+          "_id": "L302",
+          "name": "Textbook: Statistical Foundations",
+          "description": "When and where office hours will be held",
+          "module": "M1560"
+        }
+      ]
+    },
+    {
+      "_id": "M1518",
+      "name": "Measures of Central Tendency",
+      "description": "central tendency",
+      "course": "MATH1560",
+      "lessons": [
+        {
+          "_id": "L301",
+          "name": "Calculating the Mean, Median, Mode",
+          "description": "mean, median, mode",
+          "module": "M1560"
+        },
+        {
+          "_id": "L302",
+          "name": "Homework 1",
+          "description": "homework 1",
+          "module": "M1560"
+        },
+        {
+          "_id": "L303",
+          "name": "Quiz 1: Measures of Central Tendency",
+          "description": "quiz",
+          "module": "M1560"
+        }
+      ]
+    },
+    {
+      "_id": "M1519",
+      "name": "Measures of Variability",
+      "description": "variability",
+      "course": "MATH1560",
+      "lessons": [
+        {
+          "_id": "L301",
+          "name": "Calculating the Range, IQR, Variance, and Standard Deviation",
+          "description": "Range, IQR, Variance, and Standard Deviation",
+          "module": "M1560"
+        },
+        {
+          "_id": "L302",
+          "name": "Homework 2",
+          "description": "homework 2",
+          "module": "M1560"
+        },
+        {
+          "_id": "L303",
+          "name": "Quiz 2: Measures of Variability",
+          "description": "quiz",
+          "module": "M1560"
+        }
+      ]
+    },
+
+    {
+      "_id": "M1020",
+      "name": "Welcome to Intro to Life Sciences!",
+      "description": "Intro to life science",
+      "course": "SCIE1000",
+      "lessons": [
+        {
+          "_id": "L301",
+          "name": "Course Syllabus",
+          "description": "Syllabus for the course",
+          "module": "M1000"
+        },
+        {
+          "_id": "L302",
+          "name": "Office Hours and Teaching Assistants",
+          "description": "When and where office hours will be held",
+          "module": "M1000"
+        },
+        {
+          "_id": "L302",
+          "name": "Textbook: All About the Life Sciences",
+          "description": "textbook",
+          "module": "M1000"
+        }
+      ]
+    },
+    {
+      "_id": "M1021",
+      "name": "All About Plants",
+      "description": "plants",
+      "course": "SCIE1000",
+      "lessons": [
+        {
+          "_id": "L301",
+          "name": "Parts of the plant",
+          "description": "parts",
+          "module": "M1000"
+        },
+        {
+          "_id": "L302",
+          "name": "Cells of Plants",
+          "description": "cells",
+          "module": "M1000"
+        },
+        {
+          "_id": "L303",
+          "name": "Life Cycle",
+          "description": "life cycle",
+          "module": "M1000"
+        }
+      ]
+    },
+    {
+      "_id": "M1022",
+      "name": "All About Animals",
+      "description": "animals",
+      "course": "SCIE1000",
+      "lessons": [
+        {
+          "_id": "L301",
+          "name": "The Animal Kingdom, Food Pyramid, and Species",
+          "description": "animal kingdom",
+          "module": "M1000"
+        },
+        {
+          "_id": "L302",
+          "name": "Animal Cells",
+          "description": "cells",
+          "module": "M1000"
+        },
+        {
+          "_id": "L303",
+          "name": "Evolution and Reproduction",
+          "description": "evolution",
+          "module": "M1000"
+        }
+      ]
+    },
+
+    {
+      "_id": "M1123",
+      "name": "TIME TO FIND A CO-OP! - Introduction to the Course",
+      "description": "Intro to course",
+      "course": "CS1111",
+      "lessons": [
+        {
+          "_id": "L301",
+          "name": "Course Syllabus",
+          "description": "Syllabus for the course",
+          "module": "M1111"
+        },
+        {
+          "_id": "L302",
+          "name": "Office Hours and Teaching Assistants",
+          "description": "When and where office hours will be held",
+          "module": "M1111"
+        },
+        {
+          "_id": "L302",
+          "name": "Additional Resources",
+          "description": "resources",
+          "module": "M1111"
+        },
+        {
+          "_id": "L302",
+          "name": "NUworks",
+          "description": "NUworks",
+          "module": "M1111"
+        }
+      ]
+    },
+    {
+      "_id": "M1124",
+      "name": "Creating a 'Good' Resume",
+      "description": "resume",
+      "course": "CS1111",
+      "lessons": [
+        {
+          "_id": "L301",
+          "name": "How to Create a Resume: Tips and Tricks",
+          "description": "how to",
+          "module": "M1111"
+        },
+        {
+          "_id": "L302",
+          "name": "Example Resumes",
+          "description": "examples",
+          "module": "M1111"
+        },
+        {
+          "_id": "L303",
+          "name": "Upload Your Resume and Create an Appointment",
+          "description": "upload",
+          "module": "M1111"
+        }
+      ]
+    },
+    {
+      "_id": "M1125",
+      "name": "All About Interviewing",
+      "description": "animals",
+      "course": "CS1111",
+      "lessons": [
+        {
+          "_id": "L301",
+          "name": "The Behavioral Interview",
+          "description": "behavioral",
+          "module": "M1111"
+        },
+        {
+          "_id": "L302",
+          "name": "The Technical Interview",
+          "description": "technical",
+          "module": "M1111"
+        },
+        {
+          "_id": "L303",
+          "name": "Book: Cracking the Coding Interview",
+          "description": "book",
+          "module": "M1111"
+        },
+        {
+          "_id": "L303",
+          "name": "Set up a resume practice appointment!",
+          "description": "practice",
+          "module": "M1111"
+        }
+      ]
+    }
+
+  ]
