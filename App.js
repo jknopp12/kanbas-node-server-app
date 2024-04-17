c4da4cf7f5f356a724f6356248746570bc8b85d7
jknopp12 <129903306+jknopp12@users.noreply.github.com>
2024-04-17 15:20:20 -0400

    routes

--- App.js
+++ App.js
@@ -3,12 +3,12 @@ import session from "express-session";
 import express from 'express';
 import Hello from "./Hello.js"
 import Lab5 from "./Lab5.js";
-import CourseRoutes from "./Kanbas/Courses/routes.js";
-import ModuleRoutes from "./Kanbas/Modules/routes.js";
 import cors from "cors";
 import mongoose from "mongoose";
 // mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 import UserRoutes from "./Users/routes.js";
+import CourseRoutes from "./Kanbas/Courses/routes.js";
+import ModuleRoutes from "./Kanbas/Modules/routes.js";
 const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb+srv://knoppjulia12:supersecretpassword@cluster0.5gnloep.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 mongoose.connect(CONNECTION_STRING);
 const app = express();
